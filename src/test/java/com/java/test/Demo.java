e95624fa0c225615fd8a1878bf8a1752969b568f
XuYibo <xuyibo@outlook.com>
2018-11-08 11:54:20 +0800

    解决bug

--- src/test/java/com/java/test/Demo.java
+++ src/test/java/com/java/test/Demo.java
@@ -1,26 +1,23 @@
-/**
+/*
  * Project Name:ala_ssm
  * File Name:Demo.java
  * Package Name:Test
  * Date:2017年10月19日下午2:33:29
  * Copyright (c) 2017, bluemobi All Rights Reserved.
- *
  */
 
-package Test;
+package com.java.test;
+
+import org.junit.Test;
 
 /**
  * Description: <br/>
  * Date: 2017年10月19日 下午2:33:29 <br/>
- * 
+ *
  * @author chenWP
- * @version
- * @see
  */
 public class Demo {
-
-    public static void main(String[] args) {
-
+    @Test
+    public void test01() {
     }
-
 }
